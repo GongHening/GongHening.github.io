0d1561406d1422d9a574ed57146e2f95a5f958ea
GongHening <3255534851@qq.com>
2026-06-09 23:24:08 +0800

    fix(day): remove favorite button from card top-right, keep institution name
    - Remove favorite-btn and note-indicator from card template
    - Remove padding-right: 40px from card-header, title, institution, description

--- day/js/components/courseCard.js
+++ day/js/components/courseCard.js
@@ -53,15 +53,6 @@ const CourseCard = {
 
         return `
             <div class="course-card reveal${completedClass}" style="transition-delay: ${delay}ms">
-                ${hasNote ? '<div class="note-indicator" title="有笔记"></div>' : ''}
-                <button class="favorite-btn${favClass}" onclick="CourseCard.toggleFavorite('${escapeHtml(course.u)}', this)" title="${isFav ? '取消收藏' : '收藏'}">
-                    <svg class="heart-outline" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
-                    </svg>
-                    <svg class="heart-filled" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
-                    </svg>
-                </button>
 
                 <div class="course-card-header">
                     <div class="difficulty-indicator">
