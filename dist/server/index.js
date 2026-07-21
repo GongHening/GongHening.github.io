555c0f52423049df540520482c18c8078515eba4
GongHening <3255534851@qq.com>
2026-07-21 17:54:01 +0800

    Revert homepage redesign

--- dist/server/index.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env) {
-    if (!env.ASSETS) {
-      return new Response("Static assets are unavailable.", { status: 503 });
-    }
-
-    return env.ASSETS.fetch(request);
-  },
-};
